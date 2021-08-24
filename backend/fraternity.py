006c90b4950c6a3405f933d0f80c6b7d134365fa
itsbadr <badrkmc@gmail.com>
2021-08-24 16:32:15 +0500

    init fraternity

---
+++ backend/fraternity.py
@@ -0,0 +1,9 @@
+import random
+
+users = [ f"user-{i}" for i in range(90) ]
+users_schedule = dict.fromkeys(users, [])
+
+days = [0, 1, 2, 3, 4]
+
+for user in users_schedule:
+    users_schedule[user] = sorted(random.sample(days, 3))
