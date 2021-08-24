53bdbb68978b2c33037f5882626444550a531520
Julian <63492047+julianib@users.noreply.github.com>
2021-08-24 13:58:53 +0200

    availability.json

--- backend/fraternity.py
+++
@@ -1,9 +0,0 @@
-import random
-
-users = [ f"user-{i}" for i in range(90) ]
-users_schedule = dict.fromkeys(users, [])
-
-days = [0, 1, 2, 3, 4]
-
-for user in users_schedule:
-    users_schedule[user] = sorted(random.sample(days, 3))
