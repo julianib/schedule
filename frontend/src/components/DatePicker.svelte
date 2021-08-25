fa62880cb696de3712eb84ea2454dad54176b074
itsbadr <badrkmc@gmail.com>
2021-08-25 15:07:50 +0500

    make shift add / remove system

---
+++ frontend/src/components/DatePicker.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    export let date;
+
+    const dispatch = createEventDispatcher();
+
+    const removeShift = () => {
+        dispatch("removeshift", date);
+    }
+</script>
+
+<div>
+    <label for="date">Date: </label>
+    <input name="date" type="date" />
+
+    <label for="start">Start:</label>
+    <input name="start" type="number" min="0" max="23" />
+    <input name="start" type="number" min="0" max="59" />
+
+    <label for="end">End: </label>
+    <input name="end" type="number" min="0" max="23" />
+    <input name="end" type="number" min="0" max="59" />
+
+    <label for="workers"># workers</label>
+    <input name="workers" type="number" min="1" max="100" />
+    
+    <button on:click={removeShift}>X</button>
+</div>
---
+++ frontend/src/components/DatePickerController.svelte
@@ -0,0 +1,29 @@
+<script>
+    import DatePicker from "./DatePicker.svelte";
+
+    let dates = [new Date()]
+
+    const addDate = () => {
+        const newDate = new Date();
+        dates = [...dates, newDate];
+    }
+
+    const removeShift = (event) => {
+        dates = dates.filter(date => date.getTime() !== event.detail.getTime())
+    }
+
+    const sendDate = () => {
+        
+    }
+</script>
+
+<div>
+
+    {#each dates as date}
+        <DatePicker {date} on:removeshift={removeShift}/>
+    {/each}
+
+    <button on:click={addDate}>+ Add another shift</button>
+
+    <button on:click={sendDate}>Submit</button>
+</div>
