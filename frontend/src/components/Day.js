4b86ebc67dcaf4bede55c23f3bcb2b5efd3cf823
itsbadr <badrkmc@gmail.com>
2021-08-27 21:07:54 +0500

    optimize updating shift by finding index, and introduce unsaved changes

--- frontend/src/components/Day.js
+++ frontend/src/components/Day.js
@@ -5,6 +5,7 @@ import "./Day.css";
 import Shift from "./Shift";
 
 export default function Day({ day, updateDay, deleteDay }) {
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
   const [date, setDate] = useState();
   const [shifts, setShifts] = useState([]);
 
@@ -22,6 +23,7 @@ export default function Day({ day, updateDay, deleteDay }) {
     };
 
     setShifts([...shifts, newShift]);
+    setUnsavedChanges(true);
   };
 
   const saveDay = () => {
@@ -32,18 +34,16 @@ export default function Day({ day, updateDay, deleteDay }) {
     };
 
     updateDay(newDay);
+    setUnsavedChanges(false);
   };
 
   // This function will get called everytime a shift is updated on a day
   // (yes, I know, it's bad but this works @todo)
   function updateShift(updatedShiftId, key, value) {
     // First find the shift to update by its id
-    const shiftToUpdateIndex = shifts.findIndex(
-      (shift) => shift.id === updatedShiftId
-    );
+    const shiftToUpdate = shifts.find(shift => shift.id === updatedShiftId);
 
     // Get filter to update
-    const shiftToUpdate = shifts[shiftToUpdateIndex];
     shiftToUpdate[key] = value;
 
     // Then filter old shift from current shifts and then update with updated shift (shiftToUpdate)
@@ -51,6 +51,7 @@ export default function Day({ day, updateDay, deleteDay }) {
       (shift) => shift.id !== updatedShiftId
     );
     setShifts([...shiftsToUpdate, shiftToUpdate]);
+    setUnsavedChanges(true);
   }
 
   return (
@@ -88,6 +89,16 @@ export default function Day({ day, updateDay, deleteDay }) {
             + Shift
           </button>
         </div>
+        <p
+          style={{
+            color: "#09bb91",
+            fontSize: "0.8rem",
+            marginLeft: "1.2rem",
+            marginBottom: "1.2rem"
+          }}
+        >
+          {unsavedChanges ? "You have unsaved changes" : null}
+        </p>
       </div>
     </>
   );
