ee1c4205dfcb5cf637b58a0ccf278dcec60b4734
itsbadr <badrkmc@gmail.com>
2021-08-27 13:24:25 +0500

    add workers section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import CreateSchedule from "./components/CreateSchedule";
+import Workers from "./components/Workers";
+
+import "./App.css";
 
 function App() {
   return (
-      <CreateSchedule />
+      <div className="app-container">
+        <CreateSchedule />
+        <Workers />
+      </div>
   );
 }
 
---
+++ frontend/src/components/Workers.js
@@ -0,0 +1,9 @@
+import "./Workers.css";
+
+export default function Workers() {
+    return (
+        <div className="workers-container">
+            <h2 style={{ marginLeft: "1.3rem" }} className="title">Workers</h2>
+        </div>
+    )
+}
