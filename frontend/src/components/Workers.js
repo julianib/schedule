1dd8c51ad2192ccd91080acc2caa6597a156c8ae
itsbadr <badrkmc@gmail.com>
2021-08-28 18:33:36 +0500

    add workers array to support excel columns

--- frontend/src/components/Workers.js
+++ frontend/src/components/Workers.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./Workers.css";
 
 export default function Workers() {
+
+  const [workers, setWorkers] = useState([]);
+
   return (
     <div className="workers-container">
       <h2 style={{ marginLeft: "1.3rem" }} className="title">
         Workers
       </h2>
+      <ul>
+        {workers.map(worker => <li>{worker}</li>)}
+      </ul>
+      <textarea onChange={(event) => console.log(event.target.value.split("\n"))}/>
     </div>
   );
 }
