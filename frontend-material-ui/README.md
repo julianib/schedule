32b3975d95999a2c78a5a6485ea3909106e00130
Julian <julianibibo@gmail.com>
2021-08-30 17:45:19 +0200

    possible material ui rewrite

---
+++ frontend-material-ui/README.md
@@ -0,0 +1,1 @@
+# frontend rewrite with material ui
---
+++ frontend-material-ui/src/App.js
@@ -0,0 +1,14 @@
+
+
+const scheduleId = new URLSearchParams(window.location.search).get("s")
+console.log("schedule id", scheduleId)
+
+function App() {
+  return (
+      <div>
+        ok
+      </div>
+  );
+}
+
+export default App;
